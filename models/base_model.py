1d07ce1fac0a73eb377c4ab79ba1cb07fe0057ed
Richie911 <oohrichmond.k@gmail.com>
2024-02-10 21:30:55 +0000

    cleanup basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -3,10 +3,10 @@ from datetime import datetime
 import uuid
 from models import storage
 
+
 class BaseModel:
     """base class"""
-
-    def __init__(self,*args, **kwargs):
+    def __init__(self, *args, **kwargs):
         """
         initializes the base model
         Args:
@@ -16,27 +16,29 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        i = "%Y-%m-%dT%H:%M:%S.%f"
         if len(kwargs) > 0:
-            for key,value in kwargs.items():
+            for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    self.__dict__[key] = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    self.__dict__[key] = datetime.strptime(value, i)
                 else:
                     self.__dict__[key] = value
         else:
             storage.new(self)
 
     def save(self):
-        """updates the public instance attribute updated_at with the current datetime"""
-
+        """updates the public instance attribute
+        updated_at with the current datetime"""
         self.updated_at = datetime.now()
         storage.save()
 
     def __str__(self):
         """print: [<class name>] (<self.id>) <self.__dict__>"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-    
+
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__ of the instance"""
+        """returns a dictionary containing all
+        keys/values of __dict__ of the instance"""
         variable = self.__dict__
         variable["updated_at"] = self.updated_at.isoformat()
         variable["created_at"] = self.created_at.isoformat()
