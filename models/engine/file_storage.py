759f9e0234b0429b8089e02d0ee8e41f119a958e
Richie911 <oohrichmond.k@gmail.com>
2024-02-10 21:35:17 +0000

    cleanup filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 from datetime import datetime
-import os
 import json
 
-import models
 
 class FileStorage:
     """ storage class"""
@@ -11,9 +9,9 @@ class FileStorage:
         self.__file_path = 'file.json'
         self.__objects = {}
 
-    def all(self): 
+    def all(self):
         return self.__objects
-    
+
     def new(self, obj):
         self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj.__dict__
 
@@ -27,20 +25,13 @@ class FileStorage:
         if isinstance(obj, datetime):
             return obj.isoformat()
         raise TypeError(f"Type {type(obj)} not serializable")
-    
+
     def reload(self):
         """Deserialize the JSON file to objects."""
         try:
             with open(self.__file_path, 'r') as f:
                 data = json.load(f)
-                for key, value in data.items():
-                    if '__class__' in value:
-                        class_name = value['__class__']
-                        model_class = getattr(__import__('models', fromlist=[class_name]), class_name)
-                        obj = model_class(**value)
                 self.__objects = data
                 return data
         except FileNotFoundError:
             pass
-
-
